d53493c596ba990a98c1608cbda347c2a3632d2a
Peedee01 <piethein@schouten.nl>
2025-01-14 14:00:54 +0100

    changed import script

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { createClient } from "../node_modules/@supabase/supabase-js/dist/main/SupabaseClient";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
