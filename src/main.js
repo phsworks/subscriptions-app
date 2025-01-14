e64f855f5389699b3b166f862ca800cd2ffd4b89
Peedee01 <piethein@schouten.nl>
2025-01-14 17:12:57 +0100

    deployment issue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { supabase } from "./supabase/config";
-
+import { supabase } from "/src/supabase/config";
+console.log(config.js)
 // function to show all subscriptions
 async function loadSubscriptions() {
   const { data, error } = await supabase
