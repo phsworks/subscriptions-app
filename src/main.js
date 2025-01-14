b516defac5e818d0ccdfd8ad3dfd623ad5060e52
Peedee01 <piethein@schouten.nl>
2025-01-14 13:48:37 +0100

    subscriptions load

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
